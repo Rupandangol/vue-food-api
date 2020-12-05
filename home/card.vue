17fdaa1a72f984cf44d644ecf9156b7668d13f46
unknown <you@example.com>
2020-12-05 12:57:03 +0545

    GOG-29 design implementation

---
+++ home/card.vue
@@ -0,0 +1,98 @@
+<template>
+  <view class="card-container">
+    <view class="card-header-block">
+      <view class="card-image">
+        <image
+          :style="{ width: 100, height: 70, borderRadius: 4 }"
+          :source="{
+            uri:
+              'https://facebook.github.io/react-native/docs/assets/favicon.png',
+          }"
+        />
+      </view>
+      <view class="">
+        <text class="card-header"> Food of The Day </text>
+      </view>
+    </view>
+
+    <view class="card-text">
+      <text class="card-description">Description</text>
+      <view class="card-detail">
+        <text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, dicta?
+          Totam consequuntur obcaecati officiis facere deleniti iste
+          voluptatibus eaque accusantium delectus consectetur, pariatur beatae
+          accusamus cumque quo ratione sapiente laudantium.
+        </text>
+      </view>
+      <view class="see-more">
+        <touchable-opacity :on-press="onPressButton">
+          <text class="see-more-btn">See More</text>
+        </touchable-opacity>
+      </view>
+    </view>
+  </view>
+</template>
+
+
+<script>
+export default {
+  components: {},
+
+  methods: {
+    onPressButton: function () {
+      alert("clicked");
+    },
+  },
+};
+</script>
+
+
+<style lang="stylus" scoped>
+.card-container {
+  width: 100%;
+  height: 300;
+  padding: 20;
+}
+
+.card-header-block {
+  display: flex;
+  flex-direction: row;
+}
+
+.card-header {
+  margin-left: 30;
+  margin-top: 15;
+  font-size: 24;
+  font-weight: bold;
+}
+
+.card-text {
+  margin-top: 5;
+  display: flex;
+  flex-direction: column;
+}
+
+.card-description {
+  font-weight: bold;
+  margin-bottom: 5;
+}
+
+.card-detail {
+  background-color: white;
+  margin-bottom: 5;
+  padding: 5;
+}
+
+.card-detail text {
+  font-style: italic;
+}
+
+.see-more {
+  width: 70;
+  border-radius: 20;
+  background-color: #4CAF50; /* Green */
+  color: white;
+  padding: 5;
+}
+</style>
---
+++ home/list.vue
@@ -0,0 +1,177 @@
+<template>
+  <view class="list">
+    <view class="list-search">
+      <text-input
+        :style="{
+          height: 30,
+          width: 270,
+          borderColor: 'gray',
+          borderWidth: 1,
+        }"
+        v-model="text"
+      />
+      <view class="search-btn">
+        <touchable-opacity :on-press="onPressButton">
+          <text>Search</text>
+        </touchable-opacity>
+      </view>
+    </view>
+    <view>
+      <text class="list-header">List of Food</text>
+    </view>
+
+
+    <view class="list-block">
+      <!-- <flat-list
+        :data="[{ key: 'a' }, { key: 'b' }]"
+        :render-item="(item) => renderList(item)"
+      /> -->
+      <view class="food">
+        <view class="food-image">
+          <image
+            :style="{ width: 40, height: 40, borderRadius: 4 }"
+            :source="{
+              uri:
+                'https://facebook.github.io/react-native/docs/assets/favicon.png',
+            }"
+          />
+        </view>
+        <view class="food-name">
+          <text :style="{ fontSize: 15, marginTop: 6 }">Apple</text>
+        </view>
+      </view>
+      <text class="nutrient" :style="{ fontSize: 15, marginTop: 6 }"
+        >Nutrient</text
+      >
+      <view :style="{ fontSize: 10, marginTop: 2 }">
+        <touchable-opacity :on-press="onPressButton">
+          <text class="learn-more-btn" >Learn more </text>
+        </touchable-opacity>
+      </view>
+    </view>
+     <view class="list-block">
+      <!-- <flat-list
+        :data="[{ key: 'a' }, { key: 'b' }]"
+        :render-item="(item) => renderList(item)"
+      /> -->
+      <view class="food">
+        <view class="food-image">
+          <image
+            :style="{ width: 40, height: 40, borderRadius: 4 }"
+            :source="{
+              uri:
+                'https://facebook.github.io/react-native/docs/assets/favicon.png',
+            }"
+          />
+        </view>
+        <view class="food-name">
+          <text :style="{ fontSize: 15, marginTop: 6 }">Apple</text>
+        </view>
+      </view>
+      <text class="nutrient" :style="{ fontSize: 15, marginTop: 6 }"
+        >Nutrient</text
+      >
+      <view :style="{ fontSize: 10, marginTop: 2 }">
+        <touchable-opacity :on-press="onPressButton">
+          <text class="learn-more-btn" >Learn more </text>
+        </touchable-opacity>
+      </view>
+    </view>
+     <view class="list-block">
+      <!-- <flat-list
+        :data="[{ key: 'a' }, { key: 'b' }]"
+        :render-item="(item) => renderList(item)"
+      /> -->
+      <view class="food">
+        <view class="food-image">
+          <image
+            :style="{ width: 40, height: 40, borderRadius: 4 }"
+            :source="{
+              uri:
+                'https://facebook.github.io/react-native/docs/assets/favicon.png',
+            }"
+          />
+        </view>
+        <view class="food-name">
+          <text :style="{ fontSize: 15, marginTop: 6 }">Apple</text>
+        </view>
+      </view>
+      <text class="nutrient" :style="{ fontSize: 15, marginTop: 6 }"
+        >Nutrient</text
+      >
+      <view :style="{ fontSize: 10, marginTop: 2 }">
+        <touchable-opacity :on-press="onPressButton">
+          <text class="learn-more-btn" >Learn more </text>
+        </touchable-opacity>
+      </view>
+    </view>
+
+  </view>
+</template>
+
+<style>
+.list {
+  /* background-color: "rgba(255,255,0,0.4)"; */
+  width: 100%;
+  padding: 20;
+}
+.list-search {
+  display: flex;
+  flex-direction: row;
+}
+.search-btn {
+  background-color: #4caf50; /* Green */
+  color: white;
+  padding: 5;
+  margin-left: 5;
+}
+.list-header {
+  margin-top: 5;
+  margin-bottom: 5;
+  font-weight: bold;
+  font-size: 17;
+}
+
+.list-block {
+  /* background-color: aqua; */
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 8;
+}
+.food {
+  display: flex;
+  flex-direction: row;
+}
+.food-image {
+  margin-right: 4;
+}
+.learn-more-btn{
+  background-color: #dfad25; 
+  color: white;
+  padding: 5;
+  border-radius:20;
+}
+</style>
+
+<script>
+import React from "react";
+import { Text } from "react-native";
+export default {
+  components: {},
+  data: function () {
+    return {
+      isLoading: "true",
+      text: "",
+    };
+  },
+  methods: {
+    renderList: function (item) {
+      return <Text>{item.item.key}</Text>;
+    },
+    onPressButton: function () {
+      alert("Clicked");
+    },
+  },
+};
+</script>
