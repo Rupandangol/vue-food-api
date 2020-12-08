12ced4051832a322804033f304a475d1f6396ffe
Rojen-Maharjan <er.rojenmaharjan@gmail.com>
2020-12-08 19:35:07 +0545

    GOG-33 remove header/ add native base as header

--- App.vue
+++ App.vue
@@ -3,10 +3,15 @@
 </template>
 
 <script>
+import Vue from "vue-native-core";
+import { VueNativeBase } from "native-base";
 import { createAppContainer, createStackNavigator } from "vue-native-router";
 
 import Home from "./home";
 import Detail from "./detail";
+
+// registering all native-base components to the global scope of the Vue
+Vue.use(VueNativeBase);
 
 const StackNavigator = createStackNavigator(
   {
@@ -15,6 +20,16 @@ const StackNavigator = createStackNavigator(
   },
   {
     initialRouteName: "Home",
+    defaultNavigationOptions: {
+      headerMode: "none",
+      headerVisible: false,
+      header: null,
+    },
+    navigationOptions: {
+      headerMode: "none",
+      headerVisible: false,
+      header: null,
+    },
   }
 );
 
